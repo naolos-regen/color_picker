801927b2c4786bc7f9bb9eba42e7bb329d2382eb
naolos <naolos.regen@proton.me>
2026-02-15 14:27:57 +0100

    just beautiful

---
+++ build.d
@@ -0,0 +1,26 @@
+#!/usr/sbin/rdmd
+
+import std.stdio;
+import std.process;
+
+bool
+cmd (scope const (char [])[] args)
+{
+	writeln("CMD: ", args);
+	return wait(spawnProcess(args)) == 0;
+}
+
+int
+main ()
+{
+	if (!cmd(["gdc", "app.d", "-finclude-imports", "-lX11", "-lXi"]))
+	{
+		return (1);
+	}
+	if (!cmd(["./a.out"]))
+	{
+		return (1);
+	}
+	return (0);
+}
+
